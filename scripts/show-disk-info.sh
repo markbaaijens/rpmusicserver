e90db9786f00b3655511e60772e7c28ca74736f6
Mark Baaijens <mark.baaijens@gmail.com>
2021-10-15 20:24:23 +0200

    Example code for choosing a disk

---
+++ scripts/show-disk-info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export LC_ALL=C
+
+readarray -t disks < <(lsblk -b -e7 -o name,type | grep disk | awk '{print $1}')
+sd_disks=()
+for disk in "${disks[@]}"; do
+    model=$(parted /dev/$disk print | grep Model | awk '{print $2}')
+    if [[ $model == *"SD"* ]]; then
+        sd_disks+=("$disk")
+    fi
+done
+
+echo "Available disk(s):"
+counter=0
+for disk in "${sd_disks[@]}"; do
+    model=$(parted /dev/$disk print | grep Model | awk '{print $2}')
+    size=$(parted /dev/$disk print | grep Disk | awk '{print $3}')
+    echo "$counter: $model/$disk ($size)"
+    counter=$(($counter + 1))
+done
+
+unset LC_ALL
+
+read -p "Select a disk by number or press [Enter] to choose the first one " disk_choice
+
+if [ "${sd_disks[disk_choice]}" == "" ]; then
+    echo "No disk selected."
+    echo "Script ended."    
+    exit
+fi
+
+chosen_disk=${sd_disks[disk_choice]}
+echo "You have chosen: $chosen_disk"
