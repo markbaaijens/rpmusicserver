865b90bc3126d2e936bbd8289f56aa87e65b6783
Mark Baaijens <mark.baaijens@gmail.com>
2021-10-16 08:33:58 +0200

    Enhanced todos, renamed files

---
+++ scripts/burn-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Show SD-card info: lsblk | grep disk | grep mmcblk0
+# Let user choose the disk
+# Ask user again for permission; if not, bail-out
+# Download image (Raspian OS Lite)
+# Wipe the sd-card
+# Burn image
+# (not needed, is done by Raspbian on first boot) Free disk space on card 
