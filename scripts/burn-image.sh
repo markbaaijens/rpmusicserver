46616c4e25dbc0f589206528c8383b2b4ec63f4b
mikevandartel <dartello@gmail.com>
2021-10-18 13:51:41 +0200

    add user to choose from menu of storages (disks)

--- scripts/burn-image.sh
+++ scripts/burn-image.sh
@@ -58,12 +58,53 @@ apt install wget -y
 # create a working directory in the /tmp folder to operate in
 [ ! -d $workingdir ] && mkdir $workingdir
 
-# capture users choice
-read choice
-sdcard=$choice
+export LC_ALL=C
 
-echo "SD-card to burn has been detected..."
-echo "$sdcard"
+if [ -z "$(whoami | grep root)" ]
+then
+  echo "Not running as root."
+  echo "Script ended."
+  exit
+fi
+
+readarray -t disks < <(lsblk -b -e7 -o name,type | grep disk | awk '{print $1}')
+sd_disks=()
+for disk in "${disks[@]}"; do
+    model=$(parted /dev/$disk print | grep Model | awk '{print $2}')
+    if [[ $model == *"SD"* ]]; then
+        sd_disks+=("$disk")
+    fi
+done
+
+if [ "${sd_disks[0]}" == "" ]; then
+    echo "No disk available."
+    echo "Script ended." 
+    exit
+fi
+
+echo "Available disk(s):"
+counter=0
+for disk in "${sd_disks[@]}"; do
+    model=$(parted /dev/$disk print | grep Model | awk '{print $2}')
+    size=$(parted /dev/$disk print | grep Disk | awk '{print $3}')
+    echo "$counter: $model/$disk ($size)"
+    counter=$(($counter + 1))
+done
+
+unset LC_ALL
+
+read -p "Select a disk by number or press [Enter] to choose the first one " disk_choice
+
+if [ "${sd_disks[disk_choice]}" == "" ]; then
+    echo "No disk selected."
+    echo "Script ended."    
+    exit
+fi
+
+chosen_disk=${sd_disks[disk_choice]}
+echo "You have chosen: ${disks[$chosen_disk]}"
+
+sdcard=$chosen_disk
 
 # get the label and its partitions from the sd-card
 sdlabel=$(echo "$sdcard" | head -n 1)
