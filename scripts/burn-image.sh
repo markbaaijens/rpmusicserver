5ad18501ec956fb81a0a1267a29f28a1395b1d62
dartello <dartello_x@hotmail.com>
2021-10-26 22:25:21 +0200

    confirm installation by user

--- scripts/burn-image.sh
+++ scripts/burn-image.sh
@@ -53,41 +53,35 @@ fi
 echo "Available disk(s):"
 counter=0
 for disk in "${sd_disks[@]}"; do
-    model=$(parted /dev/$disk print | grep Model | cut -d " " -f2- )
-    size=$(parted /dev/$disk print | grep Disk | awk '{print $3}')
-    echo "$counter: $model /dev/$disk ($size) $([ $counter == 0 ] && echo "[default]")" # todo only the first item
+    model=$(parted /dev/$disk print | grep "Model" | cut -d " " -f2- )
+    size=$(parted /dev/$disk print | grep "Disk /dev/" | awk '{print $3}')
+    echo "$counter: $model /dev/$disk ($size) $([ $counter == 0 ] && echo "[default]")"
     counter=$(($counter + 1))
 done
 echo "q: quit"
 
-# todo q is not working
-# todo menu item for quit
 read -p "Select a disk by number or press [Enter] to choose the first one " disk_choice
-echo "$disk_choice"
-if [ "$disk_choice" == "q" ]; then
+
+if [ "$disk_choice" == "q" ] || [ "${sd_disks[disk_choice]}" == "" ]; then
     echo "No disk selected."
     cleanup_environment    
-    echo "Script ended with failure."
+    echo "Script ended."
     exit
 fi
 
 chosen_disk=${sd_disks[disk_choice]}
 echo "You have chosen: ${sd_disks[$chosen_disk]}"
-sdcard="$(lsblk | grep ${sd_disks[$chosen_disk]})"
+sdcard="$(lsblk -e7 | grep ${sd_disks[$chosen_disk]} | cut -d' ' -f1)"
 
 # get the label and its partitions from the sd-card
 sdlabel=$(echo "$sdcard" | head -n 1)
-echo "$sdlabel"
 partitions=$(echo "$sdcard" | grep -vw "$sdlabel" | grep -oE "($sdlabel)p$number_pattern")
-echo "$partitions"
-exit
-read -r -p "Do you want to start installation on $sdlabel? [yes/NO]" start_install
-if [  ! start_install == yes ]  # todo pseudo-code
-#if [ -z "$start_install" ] || [[ "$start_install" =~ ^[nN][oO]?$ ]]
+
+read -r -p "Do you want to start installation on $sdlabel? [yes/NO] " start_install
+if [ "$start_install" != "yes" ]
 then
-    echo "Script aborted."
     cleanup_environment
-    echo "Script ended with failure."
+    echo "Script ended by user."
     exit
 fi
 
