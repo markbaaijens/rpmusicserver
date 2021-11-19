a4abbca4e898f08b0258039e1cb55adbb2bb4ffa
markbaaijens <mark.baaijens@gmail.com>
2021-11-19 18:16:22 +0100

    Second attempt

--- web-interface/api/testing/test.sh
+++ web-interface/api/testing/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Note: all tests are idempotent
 
-given_a_api_when_called_root_then_correct_output_returned () {
-    output=$(curl --no-progress-meter localhost:5000)
-    if echo $output | grep "rpms" ; then echo "OK"; else echo "Fail"; fi
+curl_output_file="/tmp/curl-output.txt"
+
+given_an_api_when_called_root_then_correct_output_returned () {
+    echo "* ${FUNCNAME[0]}"
+    $(curl -s localhost:5000 > $curl_output_file 2>&1)
+    if [ "$(cat $curl_output_file | grep 'rpms')" ]; then echo "OK"; else echo "Fail"; fi
 }
 
-given_a_api_when_called_root_then_correct_output_returned
+given_an_api_when_called_root_then_correct_output_returned
 
