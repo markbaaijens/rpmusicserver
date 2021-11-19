85d1b258593fee1bde26aa66e761b314ce062c34
markbaaijens <mark.baaijens@gmail.com>
2021-11-19 18:34:03 +0100

    API test-framework (#21)
    * First attempt
    
    * Second attempt
    
    * Refactored
    
    * Deleted original test-file

--- web-interface/api/testing/test.sh
+++ web-interface/api/testing/test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Note: all tests are idempotent
 
-given_a_api_when_called_root_then_correct_output_returned () {
-    output=$(curl --no-progress-meter localhost:5000)
-    if echo $output | grep "rpms" ; then echo "OK"; else echo "Fail"; fi
+curl_output_file="/tmp/curl-output.txt"
+
+call_api () {
+    $(curl -s $1 > $curl_output_file 2>&1)
+}
+
+given_an_api_when_called_root_then_correct_output_returned () {
+    echo "* ${FUNCNAME[0]}"
+    call_api "localhost:5000"
+    if [ "$(cat /tmp/curl-output.txt | jq .name | grep 'rpms')" ]; then echo "OK"; else echo "Fail"; fi
 }
 
 given_a_api_when_called_root_then_correct_output_returned
-
