3f73510ca7df81473a9c071954ee4895e5e37d1b
markbaaijens <mark.baaijens@gmail.com>
2021-11-19 18:27:52 +0100

    Refactored

--- web-interface/api/testing/test.sh
+++ web-interface/api/testing/test.sh
@@ -3,10 +3,14 @@
 
 curl_output_file="/tmp/curl-output.txt"
 
+call_api () {
+    $(curl -s $1 > $curl_output_file 2>&1)
+}
+
 given_an_api_when_called_root_then_correct_output_returned () {
     echo "* ${FUNCNAME[0]}"
-    $(curl -s localhost:5000 > $curl_output_file 2>&1)
-    if [ "$(cat $curl_output_file | grep 'rpms')" ]; then echo "OK"; else echo "Fail"; fi
+    call_api "localhost:5000"
+    if [ "$(cat /tmp/curl-output.txt | jq .name | grep 'rpms')" ]; then echo "OK"; else echo "Fail"; fi
 }
 
 given_an_api_when_called_root_then_correct_output_returned
