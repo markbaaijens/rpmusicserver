0c0a1ce15bbe8ae0d0b98ee1cb47657b157a2f94
markbaaijens <mark.baaijens@gmail.com>
2021-11-23 22:23:55 +0100

    Extend API (#23)
    * Created GetMachineInfo
    
    * Added GetVersionInfo
    
    * Added GetUpdateLog skeleton
    
    * GetUpdateLog by dynamic array
    
    * Sample code for tail
    
    * Give lines read from file to back call
    
    * Refactor
    
    * Get a nr of lines from log file, defined in API
    
    * Test for GetUpdateLog
    
    * Generic GetLog function
    
    * Added GetUpdateLog
    
    * Added getApiList
    
    * Added GetResourceInfo (not completed)
    
    * Complete working GetResourceInfo
    
    * Added test for GetResourceInfo
    
    * Added api/GetDockerContainerList
    
    * Added api/DoUpdateServer,  api/DoRebootServer, api/DoHaltServer
    
    * Implement GetDiskList with dummy-data
    
    * Report all disks (still dummy-data)
    
    * Added todo's
    
    * Simplification of variables
    
    * Added DeviceName and MountPoint
    
    * Working implementation of GetDiskList
    
    * Implemented api/GetLmsServerInfo
    
    * Typo
    
    * Removed smartmontools from install
    
    * Removed Healthy for GetDiskList
    
    * Clear disks in GetDiskList
    
    * Uptime in GetResourceInfo now containing descriptor
    
    * Clear topProcessesByCpu + topProcessesByMemory
    
    * Correct determination od revision.json
    
    * Correct location of api-info.json
    
    * Added url in GetLmsServerInfo
    
    * Correct load-averages
    
    * Typo

--- web-interface/api/testing/test.sh
+++ web-interface/api/testing/test.sh
@@ -7,10 +7,89 @@ call_api () {
     $(curl -s $1 > $curl_output_file 2>&1)
 }
 
+given_an_api_when_called_api_list_then_root_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetApiList"
+    if [ "$(cat /tmp/curl-output.txt | jq . | grep 'Shows the API-name')" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
 given_an_api_when_called_root_then_correct_output_returned () {
-    echo "* ${FUNCNAME[0]}"
+    func_name="* ${FUNCNAME[0]}"
     call_api "localhost:5000"
-    if [ "$(cat /tmp/curl-output.txt | jq .name | grep 'rpms')" ]; then echo "OK"; else echo "Fail"; fi
+    if [ "$(cat /tmp/curl-output.txt | jq .ApiName | grep 'rpms')" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_machine_info_then_ipadress_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    test_value=$(hostname -I | awk '{print $1}')
+    call_api "localhost:5000/api/GetMachineInfo"
+    if [ "$(cat /tmp/curl-output.txt | jq .IpAddress | grep $test_value)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_machine_info_then_hostname_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    test_value=$(hostname)
+    call_api "localhost:5000/api/GetMachineInfo"
+    if [ "$(cat /tmp/curl-output.txt | jq .HostName | grep $test_value)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_machine_info_then_os_codename_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    test_value=$(lsb_release -c | awk '{print $2}')
+    call_api "localhost:5000/api/GetMachineInfo"
+    if [ "$(cat /tmp/curl-output.txt | jq .OsCodeName | grep $test_value)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_version_info_then_current_version_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetVersionInfo"
+    if [ "$(cat /tmp/curl-output.txt | jq .CurrentVersion)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_version_info_then_update_timestamp_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetVersionInfo"
+    if [ "$(cat /tmp/curl-output.txt | jq .LastUpdateTimeStamp)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_update_log_then_update_log_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetUpdateLog/10"
+    if [ "$(cat /tmp/curl-output.txt | jq .LogLines)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_api_log_then_api_log_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetApiLog/10"
+    if [ "$(cat /tmp/curl-output.txt | jq .LogLines)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_resource_info_then_memtotal_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetResourceInfo/10"
+    if [ "$(cat /tmp/curl-output.txt | jq .MemTotal)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
+}
+
+given_an_api_when_called_docker_container_list_then_dockercontainers_returned () {
+    func_name="* ${FUNCNAME[0]}"
+    call_api "localhost:5000/api/GetDockerContainerList"
+    if [ "$(cat /tmp/curl-output.txt | jq .DockerConatiners)" ]; then echo "$func_name => OK"; else echo "$func_name => Fail"; fi
 }
 
 given_an_api_when_called_root_then_correct_output_returned
+echo 
+given_an_api_when_called_api_list_then_root_returned
+echo
+given_an_api_when_called_machine_info_then_ipadress_returned
+given_an_api_when_called_machine_info_then_hostname_returned
+given_an_api_when_called_machine_info_then_os_codename_returned
+echo 
+given_an_api_when_called_version_info_then_current_version_returned
+given_an_api_when_called_version_info_then_update_timestamp_returned
+echo 
+given_an_api_when_called_update_log_then_update_log_returned
+given_an_api_when_called_api_log_then_api_log_returned
+echo 
+given_an_api_when_called_resource_info_then_memtotal_returned
+echo 
+given_an_api_when_called_docker_container_list_then_dockercontainers_returned
