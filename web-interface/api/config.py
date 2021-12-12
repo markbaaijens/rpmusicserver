edf774b5ceb35494568a67f58b318304867a96a5
markbaaijens <mark.baaijens@gmail.com>
2021-12-12 10:05:31 +0100

    [bug] Better logrotating-parameters; removed rotate-logging from api

--- web-interface/api/config.py
+++ web-interface/api/config.py
@@ -4,6 +4,4 @@ import os.path
 class Config:
     def __init__(self):
         self.LogFileName = 'api.log'
-        self.LogMaxSize = 10240000
-        self.LogBackupCount = 1
         self.Debug = True
