03057ed180a9631b81d8631797e58970a79927ba
markbaaijens <mark.baaijens@gmail.com>
2021-12-13 18:56:46 +0100

    Merge develop into master for v0.3

--- scripts/format-usbdisk.sh
+++ scripts/format-usbdisk.sh
@@ -5,17 +5,14 @@
 
 if [ -z "$(whoami | grep root)" ]; then
     echo "Not running as root."
-    echo "Script ended with failure."
     exit
 fi
 
 setup_environment() {
-    echo "Setup environment."    
     export LC_ALL=C  # Console output = English
 }
 
 cleanup_environment() {
-    echo "Cleaning up environment."
     unset LC_ALL  # Reset console output to default language
 }
 
@@ -40,16 +37,22 @@ fi
 echo "Available disk(s):"
 counter=0
 for disk in "${sd_disks[@]}"; do
-    model=$(parted /dev/$disk print | grep Model | cut -d " " -f2- )
+    model=$(parted /dev/$disk print | grep Model | cut -d " " -f3- )
     size=$(parted /dev/$disk print | grep "Disk /dev/" | awk '{print $3}')
-  	echo "$counter: $model/$disk ($size) $([ $counter == 0 ] && echo "[default]")"
+  	echo "$counter: $model/$disk ($size)"
     counter=$(($counter + 1))
 done
 echo "Q: quit"
 
-read -p "Select a disk by number or press [Enter] to choose the first one " disk_choice
+read -p "Select a disk by a number: " disk_choice
 
-if [ "${disk_choice,,}" == "q" ] || [ "${sd_disks[disk_choice]}" == "" ]; then
+if [ "${disk_choice,,}" == "q" ]; then
+    cleanup_environment    
+    echo "Script ended by user."
+    exit
+fi
+
+if [ "$disk_choice" == "" ] || [ "${sd_disks[disk_choice]}" == "" ]; then
     echo "No disk selected."
     cleanup_environment    
     echo "Script ended."
@@ -59,7 +62,32 @@ fi
 chosen_disk=${sd_disks[disk_choice]}
 echo "You have chosen: $chosen_disk"
 
-read -r -p "Do you want to continue formatting $chosen_disk? [yes/NO] " start_install
+echo "Format as:"
+echo "1: data-disk (ext4, label = usbdata)"
+echo "2: backup-disk (ext2, label = usbbackup)"
+echo "Q: quit"
+
+read -p "Select a format-type by a number: " type_choice
+
+if [ "$type_choice" == "" ]; then
+    type_choice=0
+fi
+
+if [ "${type_choice,,}" == "q" ]; then
+    cleanup_environment    
+    echo "Script ended by user."
+    exit
+fi
+
+if [ $type_choice != "1" ] && [ $type_choice != "2" ]; then
+    echo "No type selected."
+    cleanup_environment    
+    echo "Script ended."
+    exit
+fi
+echo "You have chosen: $type_choice $([ $type_choice == 1 ] && echo "=> usbdata" || echo "=> usbbackup")"
+
+read -r -p "Do you want to continue formatting '$chosen_disk' as $([ $type_choice == 1 ] && echo "'usbdata'" || echo "'usbbackup'")? [yes/NO] " start_install
 if [ "$start_install" != "yes" ]; then
     cleanup_environment
     echo "Script ended by user."
@@ -81,12 +109,12 @@ for partition in $partitions; do
     echo "Partition $partition successfully unmounted."
 done
 hdparm -z /dev/$chosen_disk
-echo "Done unmounting /dev/$chosen_disk partitions."
+echo " => done unmounting /dev/$chosen_disk partitions."
 
 echo "Start wiping $chosen_disk..."
 wipefs -a "/dev/$chosen_disk"
 hdparm -z /dev/$chosen_disk
-echo "Done wiping $chosen_disk."
+echo " => done wiping $chosen_disk."
 
 # Scripting fdisk to create partition:
 #   n = new partition
@@ -98,12 +126,19 @@ echo "Done wiping $chosen_disk."
 echo "Start creating partition on $chosen_disk..."
 echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/$chosen_disk
 hdparm -z /dev/$chosen_disk
-echo "Done creating partition on $chosen_disk."
-	
-echo "Start formatting partition on $chosen_disk..."	
-mkfs.ext4 -L 'usbdata' "/dev/$chosen_disk"1
-hdparm -z /dev/$chosen_disk
-echo "Done formatting partition on $chosen_disk."		
+echo " => done creating partition on $chosen_disk."
+
+if [ $type_choice == "1" ]; then	
+    echo "Start formatting partition on $chosen_disk as 'usbdata'..."	
+    mkfs.ext4 -L 'usbdata' "/dev/$chosen_disk"
+    hdparm -z /dev/$chosen_disk
+    echo " => done formatting partition on $chosen_disk."		
+else
+    echo "Start formatting partition on $chosen_disk as 'usbbackup'..."	
+    mkfs.ext2 -E nodiscard -L 'usbbackup' "/dev/$chosen_disk"
+    hdparm -z /dev/$chosen_disk
+    echo " => done formatting partition on $chosen_disk."		
+fi
 
 cleanup_environment
 echo "Script ended successfully."
--- web-interface/api/controller.py
+++ web-interface/api/controller.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, abort, make_response, request
 import logging
-from logging.handlers import RotatingFileHandler
+from logging import FileHandler
 import traceback
 from flask_cors import CORS
 from werkzeug.wrappers import response
+import asyncio
 
 import logic 
 from config import Config
@@ -23,11 +24,7 @@ def SetupLogger():
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
 
-        fileHandler = logging.handlers.RotatingFileHandler(
-            configObject.LogFileName, 
-            'a', 
-            configObject.LogMaxSize, 
-            configObject.LogBackupCount)
+        fileHandler = logging.FileHandler(configObject.LogFileName, 'a')
         fileHandler.setLevel(logging.DEBUG)
         fileHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
         logger.addHandler(fileHandler)
@@ -117,6 +114,17 @@ def GetVersionInfo():
     
     return BuildResponse(HTTP_OK, jsonify(info), request.url)
 
+@app.route('/api/GetBackupInfo', methods=['GET'])
+def GetBackupInfo():
+    try:
+        info = logic.GetBackupInfo()
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)
+
 @app.route('/api/GetUpdateLog/<int:nrOfLines>', methods=['GET'])
 def GetUpdateLog(nrOfLines):
     try:
@@ -149,6 +157,28 @@ def GetTranscoderLog(nrOfLines):
         return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
     
     return BuildResponse(HTTP_OK, jsonify(info), request.url)    
+
+@app.route('/api/GetBackupLog/<int:nrOfLines>', methods=['GET'])
+def GetBackupLog(nrOfLines):
+    try:
+        info = logic.GetLog('/media/usbdata/rpms/logs/backup.log', nrOfLines)
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)    
+
+@app.route('/api/GetBackupDetailsLog/<int:nrOfLines>', methods=['GET'])
+def GetBackupDetailsLog(nrOfLines):
+    try:
+        info = logic.GetLog('/media/usbdata/rpms/logs/backup-details.log', nrOfLines)
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)        
 
 @app.route('/api/SetTranscoderSettingSourceFolder', methods=['POST'])
 def SetTranscoderSettingSourceFolder():
@@ -276,7 +306,20 @@ def GetDiskList():
 @app.route('/api/DoRebootServer', methods=['POST'])
 def DoRebootServer():
     try:
-        info = logic.DoRebootServer()
+        asyncio.run(logic.DoRebootServer())
+        info = { "Message": "Server is rebooting" }
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)
+
+@app.route('/api/DoBackupServer', methods=['POST'])
+def DoBackupServer():
+    try:
+        asyncio.run(logic.DoBackupServer())
+        info = { "Message": "Backup has been started" }        
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
@@ -287,7 +330,8 @@ def DoRebootServer():
 @app.route('/api/DoHaltServer', methods=['POST'])
 def DoHaltServer():
     try:
-        info = logic.DoHaltServer()
+        asyncio.run(logic.DoHaltServer())
+        info = { "Message": "Server is halting" }
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
@@ -298,7 +342,8 @@ def DoHaltServer():
 @app.route('/api/DoUpdateServer', methods=['POST'])
 def DoUpdateServer():
     try:
-        info = logic.DoUpdateServer()
+        asyncio.run(logic.DoUpdateServer())
+        info = { "Message": "Server is updating" }        
     except Exception as e:
         logger.error(e)
         logger.error(traceback.format_exc())
