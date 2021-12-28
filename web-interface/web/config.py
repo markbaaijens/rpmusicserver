59f768ea7aac5634e99e32ccf0fe89c36ff5e333
markbaaijens <mark.baaijens@gmail.com>
2021-12-28 16:39:04 +0100

    Implemented web.log

--- web-interface/web/config.py
+++ web-interface/web/config.py
@@ -4,7 +4,7 @@ import os.path
 class Config:
     def __init__(self):
         self.AppTitle = 'RP Music Server'
-        self.LogFileName = 'api.log'
+        self.LogFileName = 'web.log'
         self.Debug = True    
         self.ApiRootUrl = 'http://localhost:5000'
     
--- web-interface/web/controller.py
+++ web-interface/web/controller.py
@@ -24,21 +24,22 @@ from converters import ConvertToTwoDecimals, ConvertBooleanToText
 app = Flask(__name__)
 logger = logging.getLogger()
 
-'''
-if not logger.handlers:
-    logger.setLevel(logging.DEBUG)
+def SetupLogger():
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
 
-    fileHandler = logging.handlers.RotatingFileHandler(
-        app.config['LOG_FILE_NAME'], 'a', app.config['LOG_MAX_SIZE'], app.config['LOG_BACKUP_COUNT'])
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-    logger.addHandler(fileHandler)
+        fileHandler = logging.FileHandler(configObject.LogFileName, 'a')
+        fileHandler.setLevel(logging.DEBUG)
+        fileHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+        logger.addHandler(fileHandler)
 
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.DEBUG)
-    consoleHandler.setFormatter(logging.Formatter('%(message)s'))
-    logger.addHandler(consoleHandler)
-'''
+        # By default, console logging is disabled once logger is activated; to still see console messages, 
+        # a consoleHandler must be created
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setLevel(logging.DEBUG)
+        consoleHandler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(consoleHandler)
+    pass
 
 @app.route('/', methods=['GET'])
 def index():
@@ -73,8 +74,26 @@ def index():
     )
 
 if __name__ == '__main__':
-    logger.debug('App Started')
-    app.run(port=5001, debug=True)  # auto-reload, only localhoast
-#    app.run(host='0.0.0.0', port=5001)  # public server, reachable from remote
-    logger.debug('App Stopped')
+    import argparse
+    parser = argparse.ArgumentParser(description='Controller for RP Music Server API')
+    parser.add_argument('--logfile', type=str,  help="file where log is stored", nargs=1) 
+    parser.add_argument('-p', '--production', help="start a production server", action="store_true")        
+
+    args = parser.parse_args()
+
+    configObject.Debug = not args.production
+
+    if (args.logfile != None) and (args.logfile[0] != ''):
+        configObject.LogFileName = args.logfile[0]
+
+    SetupLogger()       
+    logger.info('Log to: ' + configObject.LogFileName)
+
+    if configObject.Debug:
+        logger.info('API started - debug')
+        app.run(port=1080, debug=True)  # auto-reload on file change, only localhost
+    else:
+        logger.info('API started - production')
+        app.run(host='0.0.0.0', port=80)  # public server, reachable from remote
+    logger.info('API stopped')
 
