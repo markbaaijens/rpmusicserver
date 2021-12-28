9014ac0b47b91001fde5f1103cbd17d820e2aec5
markbaaijens <mark.baaijens@gmail.com>
2021-12-28 16:30:38 +0100

    Some cleanup, erase 'book' code

--- web-interface/web/converters.py
+++ web-interface/web/converters.py
@@ -1,5 +1,3 @@
-from enums import BookType
-
 def ConvertBooleanToText(booleanValue):
     if booleanValue: 
         return 'Yes'
@@ -9,18 +7,3 @@ def ConvertBooleanToText(booleanValue):
 
 def ConvertToTwoDecimals(decimalValue):
     return '%.2f' % decimalValue
-
-def ConvertEnumBookTypeToDescription(bookType):
-    description = 'Unknown'
-    if bookType == BookType.Unknown:
-        description = 'Unknown'
-    else:
-        if bookType == BookType.Fiction:
-            description = 'Fiction'
-        else:
-            if bookType == BookType.NonFiction:
-                description = 'Non-fiction'
-            else:
-                if bookType == BookType.Educational:
-                    description = 'Educational'
-    return description + ' (' + str(bookType) + ')'
--- web-interface/web/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class BookType(Enum):
-    Unknown = 0
-    Fiction = 1
-    NonFiction = 2
-    Educational = 3
