d7c4ecb9ffc87466cc984caf1a3f87726e1bcc74
markbaaijens <mark.baaijens@gmail.com>
2023-04-01 12:04:33 +0200

    Bug: resolve empty values for int-fields

--- web-interface/web/forms.py
+++ web-interface/web/forms.py
@@ -25,11 +25,15 @@ class EditTranscoderForm(FlaskForm):
     # show custom messages; this is overruled by HTML5
     # Name of field validators must be in this exact format: validate_<variable>
     def validate_oggQuality(self, field):
+        if field.data == None:
+            field.data = 0
         if not int(field.data) in [0, 1, 2, 3, 4, 5]:  
             raise ValidationError('Value must be between 0 and 5')
         pass
 
     def validate_mp3Bitrate(self, field):
+        if field.data == None:
+            field.data = 0        
         if not int(field.data) in [0, 128, 256, 384]:  
             raise ValidationError('Value must be 0, 128, 256 or 384')
         pass
