368e64c91bd6dea99c892d95ebd9aa392f54be7f
markbaaijens <mark.baaijens@gmail.com>
2023-04-19 08:11:03 +0200

    Typo in message (#281)

--- web-interface/web/controller.py
+++ web-interface/web/controller.py
@@ -295,7 +295,7 @@ def DoUpdateRpms():
         logger.error(traceback.format_exc())
         apiMessage = []
 
-    flash('Update is in progress. Refresh his page after 2 minutes.')
+    flash('Update is in progress. Refresh this page after 2 minutes.')
 
     return redirect('/')
 
