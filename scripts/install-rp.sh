4511f22f2fe1a68e44f167e01f7494c1219d5f2e
markbaaijens <mark.baaijens@gmail.com>
2024-11-20 13:18:45 +0100

    Autoconnect backup disk (#369)
    * Refactored AppendDiskInfo()
    
    * Automount usbbackup
    
    * Typo

--- scripts/install-rp.sh
+++ scripts/install-rp.sh
@@ -48,23 +48,20 @@ echo "Cleanup /usr/local/bin"
 rm -rf /usr/local/bin/*
 echo "... cleaned up."
 
+# By always delete existing lines in fstab, we can easily implement
+# a different strategy later, if needed
+
 echo "Adding line for usbdata-disk to /etc/fstab"
-if [ ! "$(grep "LABEL=usbdata" /etc/fstab)" ]; then
-    # auto,nofail: server starts even when harddisk is not present
-    /bin/sh -c 'echo "LABEL=usbdata /media/usbdata ext4 auto,nofail 0 0" >> /etc/fstab'
-    echo "... line added."
-else
-    echo "... line is already present."    
-fi
+sed -i '/usbdata/d' /etc/fstab
+# auto,nofail: server starts even when harddisk is not present
+/bin/sh -c 'echo "LABEL=usbdata /media/usbdata ext4 auto,nofail 0 0" >> /etc/fstab'
+echo "... line added."
 
 echo "Adding line for usbbackup-disk to /etc/fstab"
-if [ ! "$(grep "LABEL=usbbackup" /etc/fstab)" ]; then
-    # auto,nofail: server starts even when harddisk is not present
-    /bin/sh -c 'echo "LABEL=usbbackup /media/usbbackup ext4 auto,nofail 0 0" >> /etc/fstab'
-    echo "... line added."
-else
-    echo "... line is already present."    
-fi    
+sed -i '/usbbackup/d' /etc/fstab
+# auto,nofail: server starts even when harddisk is not present; x-systemd.automount: automounting usbbackup
+/bin/sh -c 'echo "LABEL=usbbackup /media/usbbackup ext4 auto,nofail,x-systemd.automount 0 0" >> /etc/fstab'
+echo "... line added."
 
 mount -a
 
@@ -142,7 +139,7 @@ install_bin_file start-web
 install_bin_file generate-samba-conf
 
 # By always delete existing lines in crontab, we can easily implement
-# a different crontab-strategy later, if needed
+# a different strategy later, if needed
 echo "Adding line to transcode in /etc/crontab..."
 sed -i '/transcode/d' /etc/crontab
 /bin/sh -c 'echo "20  * * * * root transcode" >> /etc/crontab'
