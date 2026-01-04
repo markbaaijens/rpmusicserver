48f1acf70a20204335bdf70b1c0d5eee46d9be8f
markbaaijens <mark.baaijens@gmail.com>
2026-01-04 18:31:26 +0100

    Log update from install (#467)
    * Log install-progress into install.log
    
    * Show install-log
    
    * Typo
    
    * Renamed install.log to update-details.log

--- web-interface/api/controller.py
+++ web-interface/api/controller.py
@@ -155,6 +155,17 @@ def GetUpdateLog(nrOfLines):
     
     return BuildResponse(HTTP_OK, jsonify(info), request.url)
 
+@app.route('/api/GetUpdateDetailsLog/<int:nrOfLines>', methods=['GET'])
+def GetUpdateDetailsLog(nrOfLines):
+    try:
+        info = logic.GetLog('/var/log/update-details.log', nrOfLines)
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)
+
 @app.route('/api/GetMusicCollectionInfo', methods=['GET'])
 def GetMusicCollectionInfo():
     try:
