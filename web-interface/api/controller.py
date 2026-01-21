bea1c16b9543a80cf8be041fc4793ec43b2aac7a
markbaaijens <mark.baaijens@gmail.com>
2026-01-21 16:37:03 +0100

    Get transcoded files (not working)

--- web-interface/api/controller.py
+++ web-interface/api/controller.py
@@ -188,6 +188,17 @@ def GetFlacHealthInfo():
     
     return BuildResponse(HTTP_OK, jsonify(info), request.url)      
 
+@app.route('/api/GetTranscodedFiles/<int:nrOfLines>', methods=['GET'])
+def GetTranscodedFiles(nrOfLines):
+    try:
+        info = logic.GetTranscodedFiles(nrOfLines)
+    except Exception as e:
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return BuildResponse(HTTP_BAD_REQUEST, jsonify({'message': str(e)}), request.url)
+    
+    return BuildResponse(HTTP_OK, jsonify(info), request.url)
+
 @app.route('/api/GetApiLog/<int:nrOfLines>', methods=['GET'])
 def GetApiLog(nrOfLines):
     try:
